bb9430f8451d711959cce613026cdf3cd3e5bf2f
Giorgia Sambrotta <g.sambrotta@gmail.com>
2013-03-02 20:57:15 +0100

    crate the book list and style it a bit

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function(){
+        $(".button").click(function() {
+        var query = $("#query").val();
+            $.getJSON('https://api.readmill.com/v2/books/search.json?query='+query+'&client_id=3c3e949307f81c6445f04491b5843609&callback=?', function(data) {
+                $.each(data.items, function(i, element){
+                    var link = $("<a href=\"#" + "\">"+ " " +element.book.author+" " +element.book.title+" " +element.book.story+"</a>");
+                    $('.wrap').append(link).append("<br />"+"<br />");
+                    link.click(function(){
+                        console.log(element.book.id);
+                        $.get('/home/search', element.book.id);
+                    });
+                });
+                
+                
+            });
+        });
+    })
