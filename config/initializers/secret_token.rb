05f373bab6bd8f415e1a383737b07d5e060a05e5
Josefine Harzmann <jharzmann@gmail.com>
2013-03-02 14:34:37 +0100

    initial stuff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookBeat::Application.config.secret_token = 'c0245387a49adaf84eaae4f2e570351c5b5d12a1bc638eb8eb805b344c696a18b4aa61ceb80a788ae34392ceed07cc7cce5b7c1e73adf6f86571f6ebc9b1c295'
